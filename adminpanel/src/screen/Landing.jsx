807b7d5eebcc9f8f921ee97c044cab36df68a0da
Apple retina <appleretina@Apples-MacBook-Pro-6.local>
2023-07-15 15:29:30 +0530

    landing background fix

--- adminpanel/src/screen/Landing.jsx
+++ adminpanel/src/screen/Landing.jsx
@@ -6,9 +6,9 @@ const Home = () => {
   // getting users data from context
   const userData = useContext(UserContext);
   return (
-    <div style={{ position: "relative" }}>
+    <div style={{ position: "relative", width: "100vw" }}>
       {/* Svg for curve background */}
-      <div>
+      <div style={{ width: "100%" }}>
         <svg viewBox='0 0 450 200'>
           <linearGradient id='linear-gradient'>
             <stop stopColor='#3c31dd' stopOpacity='10%' />
@@ -16,7 +16,7 @@ const Home = () => {
           </linearGradient>
           <path
             d='M0, 100 C100, 200 300,
-                0 500, 150 L500, 00 L0, 0 Z'
+                0 500, 150 L500, 00 L0 , 0 Z'
             style={{ fill: "url(#linear-gradient)" }}></path>
         </svg>
       </div>
